3e2cbd030cfbdb6b971d58431c08cf37c8b47fc4
wooara <wknowpark797@gmail.com>
2023-01-16 23:09:25 +0900

    외부 라이브러리 axios, vuetify 설치 및 연동

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -1,5 +1,14 @@
 module.exports = {
     head: {
         title: 'NodeBird'
+    },
+    modules: [
+        '@nuxtjs/axios'
+    ],
+    buildModules: [
+        '@nuxtjs/vuetify'
+    ],
+    vuetify: {
+
     }
 }
